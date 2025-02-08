64a902c5c52d1b6304f27b933517f440cea73d3d
firdaus <rivakanzf@gmail.com>
2025-02-08 20:44:56 +0700

    input

---
+++ dummy/hotels.js
@@ -0,0 +1,26 @@
+export const hotels = [
+    {
+        name: "Hotel Indonesia Kempinski Jakarta",
+        location: "Thamrin, Jakarta",
+        city: "Jakarta",
+        rating: 9,
+        star: 5,
+        price: '2500000'
+    },
+    {
+        name: "Hotel Indonesia Kempinski Jakarta",
+        location: "Thamrin, Jakarta",
+        city: "Jakarta",
+        rating: 9,
+        star: 5,
+        price: '2500000'
+    },
+    {
+        name: "Hotel Indonesia Kempinski Jakarta",
+        location: "Thamrin, Jakarta",
+        city: "Jakarta",
+        rating: 9,
+        star: 5,
+        price: '2500000'
+    }
+]
